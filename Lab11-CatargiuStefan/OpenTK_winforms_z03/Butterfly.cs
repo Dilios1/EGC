c570295f72a06ce931d85d1cb15cc143b9788815
Stefan Catargiu <98192537+Dilios1@users.noreply.github.com>
2024-01-23 16:01:03 +0200

    Add files via upload

---
+++ Lab11-CatargiuStefan/OpenTK_winforms_z03/Butterfly.cs
@@ -0,0 +1,27 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_winforms_z03
+{
+    public class Butterfly
+    {
+        private float wingRotationAngle = 0.0f;
+        private float wingRotationSpeed = 0.1f;
+
+        public void Update()
+        {
+            // Logica de animație: rotație periodica a aripilor
+            wingRotationAngle += wingRotationSpeed;
+            if (wingRotationAngle > 360.0f)
+            {
+                wingRotationAngle -= 360.0f;
+            }
+        }
+
+        
+    }
+}
